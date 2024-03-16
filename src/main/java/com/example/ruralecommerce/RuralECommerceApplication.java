380ddb83d7ef8f1d918ccdfb7615f9075af50118
Hrzmxn <1316510990@qq.com>
2024-03-16 09:58:03 +0800

    1

---
+++ src/main/java/com/example/ruralecommerce/RuralECommerceApplication.java
@@ -0,0 +1,13 @@
+package com.example.ruralecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RuralECommerceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RuralECommerceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/ruralecommerce/RuralECommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ruralecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RuralECommerceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
